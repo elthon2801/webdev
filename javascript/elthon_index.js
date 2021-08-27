e063df7648da55651f35282b1d1aacba9bdaf6f0
elthon2801 <ejfcayetano.cca@gmail.com>
2021-08-27 19:42:45 +0800

    asfsdgdgvs

--- javascript/elthon_index.js
+++ javascript/elthon_index.js
@@ -4,6 +4,7 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
@@ -14,5 +15,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 2000);
 }
